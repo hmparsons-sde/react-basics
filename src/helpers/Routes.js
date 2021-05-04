cd5668c0ab85309880f2d46322b4c3ffd11775ac
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-05-03 21:08:06 -0500

    fixed nav links

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AddStudent from '../views/AddStudent';
 import Home from '../views/Home';
 import Students from '../views/Students';
 import SingleStudent from '../views/SingleStudent';
 
+// The PrivateRoute function is creating a private route and returing the specified route based on the props
+// We specify the specific props we want to use in the routeChecker and pass the rest with the spread
+const PrivateRoute = ({ component: Component, user, ...rest }) => {
+  // when we call this function in the return, it is looking for an argument. `props` here is taco.
+  const routeChecker = (taco) => (user ? <Component {...taco} user={user} /> : <Redirect to={{ pathname: '/', state: { from: taco.location } }} />);
+  // this render method is one we can use instead of component. Since the components are being dynamically created, we use render. Read the docs for more info: https://reactrouter.com/web/api/Route/render-func
+  // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func,
+  user: PropTypes.any,
+};
 export default function Routes({ user, students, setStudents }) {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={() => <Home user={user} />} />
-        <Route exact path="/students" component={() => <Students students={students} setStudents={setStudents} />} />
-        <Route path="/student/:id" component={SingleStudent} />
-        <Route path="/add-student" component={() => <AddStudent setStudents={setStudents} />} />
+        <PrivateRoute exact user={user} path="/students" component={() => <Students students={students} setStudents={setStudents} />} />
+        <PrivateRoute user={user} path="/student/:id" component={SingleStudent} />
+        <PrivateRoute path="/add-student" component={() => <AddStudent user={user} setStudents={setStudents} />} />
       </Switch>
     </div>
   );
